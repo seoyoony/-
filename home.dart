2c1eb66fce63f341450651722d16e7c002c9403e
seoyoony <165654039+seoyoony@users.noreply.github.com>
2025-03-07 17:30:39 +0900

    text02_app

--- home.dart
+++ home.dart
@@ -7,59 +7,52 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Column & Row Testing"
-        ),
-        backgroundColor: Colors.black,
-        foregroundColor: Colors.white,
+        title: Text('Text with Column and Row'),
       ),
-      body: Center( 
-        child: Column(
+      body: Center(
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("유비"),
-            Padding(
-              padding: const EdgeInsets.all(20),
-              child: Text("관우"),
-            ),
-            Text("장비"),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Padding(
-                  padding: const EdgeInsets.all(40.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("캐시"),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text("James"),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text("cathy"),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text("Martin"),
-                          )
-                        ],
-                      ),
-                      Text("CATHY")
-                    ],
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.email_rounded
                   ),
                 ),
-              ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.account_balance
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.account_circle
+                  ),
+                )
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.all(20),
-              child: Text("123"),
-            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text("James"),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text("Cathy"),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text("Kenny"),
+                ),
+              ],
+            )
           ],
         ),
       ),
